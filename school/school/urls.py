d665a0e57f20463c8b336cdcf350003621556a59
ArAr <86514824+CoconatsuMiru@users.noreply.github.com>
2026-03-12 04:15:03 +0000

    Lab act

--- school/school/urls.py
+++ school/school/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("theme.urls"), name='home'),
-
+    path('cms/', include("highschool.urls"))
 ]
 
 if settings.DEBUG:
